ecdebfaa5086ef268efaaa956ea8115543c68d24
allison d manzo <adm52@afsconnect1.njit.edu>
2017-03-28 20:11:27 -0400

    added code for hw

--- ch10_ex1/index.php
+++ ch10_ex1/index.php
@@ -32,24 +32,40 @@ switch ($action) {
         $due_date_s = filter_input(INPUT_POST, 'due_date');
 
         // make sure the user enters both dates
+	if (empty($invoice_date_f)) {
+		$message = 'You must enter an invoice date.';
+		break;
+	}
+	
+	if (empty($due_date_s)) {
+		$message = 'You must enter a due date.';
+	}
 
         // convert date strings to DateTime objects
+	echo $invoice_date_f->format('Y-m-d a\t\ H:i:s');
+	echo $due_date_s->format('Y-m-d a\t\ H:i:s');
+
         // and use a try/catch to make sure the dates are valid
+	$valid_date = checkdate($M, $D, $Y);
+	$valid_time = checktime($H, $i, $s);
 
         // make sure the due date is after the invoice date
+	if ($due_date < $invoice_date_f) {
+		echo 'The due date must be after the invoice date';
+	}
 
         // format both dates
-        $invoice_date_f = 'not implemented yet';
-        $due_date_f = 'not implemented yet'; 
+        $invoice_date_f = date('Y-m-d a\t\ H:i:s');
+        $due_date_f = date('Y-m-d a\t\ H:i:s');; 
         
         // get the current date and time and format it
-        $current_date_f = 'not implemented yet';
-        $current_time_f = 'not implemented yet';
+        $current_date_f = date(Y-m-d);
+        $current_time_f = time(H:i:s);
         
         // get the amount of time between the current date and the due date
         // and format the due date message
-        $due_date_message = 'not implemented yet';
-
+        $time_span = $current_date_f->diff($due_date_f);
+	echo $time_span->format('%R%dd %H:%I:%SH')
         break;
 }
 include 'date_tester.php';
